a0746f12044887c2911768d5a51a0905b8a7b9b2
Jordany Rosas <jordanyryan@gmail.com>
2017-05-09 22:24:26 -0400

    Moved youtube-search into app

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
-
+import VideoList from './components/video_list'
 const YOUTUBE_API_KEY = 'AIzaSyBi-tUrrZWDY9Q7rMgbC-IuvJXrX-kbwL4'
 
 YTSearch({key: YOUTUBE_API_KEY, term: 'runescape'}, function(data) {
@@ -10,11 +10,24 @@ YTSearch({key: YOUTUBE_API_KEY, term: 'runescape'}, function(data) {
 })
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      videos: [],
+    }
+
+    YTSearch({key: YOUTUBE_API_KEY, term: 'Breaking Bad'}, videos => {
+    this.setState({ videos })
+    })
+  }
 
   render() {
+    console.log(this.state.videos)
   return (
     <div>
       <SearchBar />
+      <VideoList />
     </div>
     )
 
